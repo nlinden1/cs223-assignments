0b489b13ce0a078fa4e575572c6269b9f8d26967
Neville Linden <nevillelinden@nevilles-mbp.haverford.edu>
2023-02-14 12:14:35 -0500

    A04 complete

--- A03/slow.c
+++ A03/slow.c
@@ -11,20 +11,28 @@
 
 int main()
 {
-    char word[128];
-    char dots[4] = ".";
+    char word[32];
+    char* slowspeech;
+    char dots[2] = ".";
     int length;
     printf("it's tree time: welcome to the ent speech generator.\n\n");
     printf("please enter a word: ");
     scanf("%[^\n]%*c", word);
     printf("please enter a pause-length: ");
     scanf("%d", &length);
+    //slowspeech's size must be the length of the phrase itself + length * the number of dots
+    slowspeech = malloc(strlen(word) * (length + 1));
+
     for(int i = 0; i < strlen(word); i++) {
-        printf("%c", word[i]);
+        //printf("\n%c\n\n", word[i]);
+        strncat(slowspeech, &word[i], 1);
         for(int j = 0; j < length; j++) {
-            printf("%s", dots);
+            strcat(slowspeech, dots);
         }
     }
     
+    printf("%s\n", slowspeech);
+
+    free(slowspeech);
     return 0;
 }
